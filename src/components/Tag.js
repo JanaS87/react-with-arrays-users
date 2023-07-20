e24c53763b05230de1625013286ac11cbff4a6d9
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-07-20 16:22:58 +0200

    add function, array, fix tag

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,5 +1,7 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const isAdmin = tag === "admin";
+  const tagClassName = isAdmin ? "tag Tag--highlight" : "tag";
+  return <li className={tagClassName}>{tag}</li>;
 }
