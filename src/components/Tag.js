25697153b34f39976077c85716a15b641c46b186
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-07-20 16:26:45 +0200

    fix tag-color

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,6 +2,6 @@ import "./Tag.css";
 
 export default function Tag({ tag }) {
   const isAdmin = tag === "admin";
-  const tagClassName = isAdmin ? "tag Tag--highlight" : "tag";
+  const tagClassName = isAdmin ? "tag tag--highlight" : "tag";
   return <li className={tagClassName}>{tag}</li>;
 }
